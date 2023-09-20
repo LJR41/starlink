c58972c76fc7bd7c8b3e1a18f2b666dae1414afa
Jetmishemoto <Daadoniwp92@gmx.com>
2023-09-19 21:57:05 -0500

    Added Simple AI Chase function

--- Assets/StarterAssets/Enemys/SmallEnemy/Enemy.cs
+++ Assets/StarterAssets/Enemys/SmallEnemy/Enemy.cs
@@ -3,21 +3,70 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.Serialization;
 using UnityEngine;
+using UnityEngine.AI;
+
+
 
 public class Enemy : MonoBehaviour
 {   
     [SerializeField] float health;
 
+    public Transform player;
 
-    public void Awake()
+
+    public Transform patrolRoute;
+    public List<Transform> locations;
+
+
+    private int locationIndex = 0;
+    private NavMeshAgent agent;
+
+
+    private void Awake()
     {
+        InitializePatrolRoute();
+        player = GameObject.Find("Player").transform;
+        if (patrolRoute == null) patrolRoute = patrolRoute.transform.Find("PatrolRoute");
+        agent = GetComponent<NavMeshAgent>();
+        MoveToNextPatrolLocation();
+    }
+
+     void Start()
+    {
+        
+        
+       
+    }
+
+
+     void Update()
+    {
+        if (agent.remainingDistance < 0.2f && !agent.pathPending) MoveToNextPatrolLocation();
         
     }
 
-    public void Update()
+
+
+    void MoveToNextPatrolLocation()
     {
-        
+        if (locations.Count == 0) return;
+
+        agent.destination = locations[locationIndex].position;
+        locationIndex = (locationIndex + 1) % locations.Count;
+
     }
+
+    void InitializePatrolRoute()
+    {
+        foreach ( Transform child in patrolRoute)
+        {
+            locations.Add(child);
+
+        }
+
+    }
+
+
     public void TakeDamage(float damage)
     {
         if(health > 0)
@@ -36,5 +85,16 @@ public class Enemy : MonoBehaviour
     void EnemyDeath()
     {
         UnityEngine.Debug.Log("Death");
+    }
+
+    public void OnTriggerEnter(Collider otherCollider)
+    {
+        if(otherCollider.name == "Player")
+        {
+            agent.destination = player.position;
+            print("Player Found Attacking!");
+        }
+
+        
     }
 }
